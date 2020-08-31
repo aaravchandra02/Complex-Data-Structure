1f165f01ccae2e18f66962d9574e6a92067d6930
Aarav Chandra <aaravchandra02@gmail.com>
2020-08-30 21:24:05 -0700

    added comments

--- graph/vertex.py
+++ graph/vertex.py
@@ -1,10 +1,13 @@
 class Vertex:
+    # stores some data and a dictionary of edges
     def __init__(self, value):
         self.value = value
         self.edges = {}
 
+    # adds egde and also a weight as a default parameter
     def add_edge(self, vertex, weight=0):
         self.edges[vertex] = weight
 
+    # function to return the vertices
     def get_edges(self):
         return self.edges.keys()
