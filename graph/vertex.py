f3d3ff2640b3b048b3e0ee0c51840c846bc8c724
Aarav Chandra <aaravchandra02@gmail.com>
2020-08-31 09:56:41 -0700

    added prints and testcases

--- graph/vertex.py
+++ graph/vertex.py
@@ -6,7 +6,15 @@ class Vertex:
 
     # adds egde and also a weight as a default parameter
     def add_edge(self, vertex, weight=0):
+        print(f"Adding edge to {vertex}")
+        print(f"Adding edge to {vertex}")
+        self.value = vertex
         self.edges[vertex] = weight
+
+    # define .add_edge() here
+    def add_edges(self, vertex):
+
+        self.value = vertex
 
     # function to return the vertices
     def get_edges(self):
@@ -14,3 +22,11 @@ class Vertex:
 
 
 station = Vertex("Cronk")
+grand_central = Vertex('Grand Central Station')
+forty_second_street = Vertex('42nd Street Station')
+
+print(grand_central.get_edges())
+
+grand_central.add_edge(forty_second_street.value)
+
+print(grand_central.get_edges())
